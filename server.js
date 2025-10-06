68e7e4ed5a30c3620f8898fbee633a077dc93ac7
khoabkhn-byte <khoabkhn@gmail.com>
2025-10-06 08:21:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -210,17 +210,10 @@ app.post('/api/results', (req, res) => {
   res.status(201).json(newResult);
 });
 
-const express = require("express");
-const cors = require("cors");
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on https://tn-j0j4.onrender.com`);
 });
+
 
 
 
